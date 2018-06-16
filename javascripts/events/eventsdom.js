94b661a64a4120c89bfedf9ae93a5c85df9492eb
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-16 12:43:36 -0500

    got correct cards printing

--- javascripts/events/eventsdom.js
+++ javascripts/events/eventsdom.js
@@ -1,6 +1,6 @@
 const domStrang = (eventsArray) => {
+  let strang = '';
   eventsArray.forEach((event) => {
-    let strang = '';
     strang +=  `<div class="col-sm-12">`;
     strang +=   `<div class="thumbnail">`;
     strang +=     `<h3>${event.eventName}</h3>`;
@@ -8,8 +8,8 @@ const domStrang = (eventsArray) => {
     strang +=     `<h4>${event.eventDate}</h3>`;
     strang +=     `<p><a href="#" id="event-save-button" class="btn btn-primary" role="button">Save</a> <a href="#" id="event-edit-button" class="btn btn-warning" role="button">Edit</a></p>`;
     strang +=    `</div>`;
-    printToDom('#events-card-holder', strang);
   });
+  printToDom('#events-card-holder', strang);
 };
 
 const printToDom = (id, string) => {
