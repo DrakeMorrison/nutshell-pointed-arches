fa5eec61419d346df1a59b9187bea0cd950cce54
Rita Patrick <rita.patrick1007@gmail.com>
2018-06-19 01:19:58 -0500

    deleted commented code

--- javascripts/articles/article-main.js
+++ javascripts/articles/article-main.js
@@ -1,11 +1,9 @@
 const {modalBtn,} = require('./article-events');
 const {saveArticleEvent,} = require('./article-events');
-// const {getAllArticlesEvent,} = require('./article-events');
 const {deleteArticleFromFirebase,} = require('./article-events');
 function startArticles () {
   modalBtn();
   saveArticleEvent();
-  // getAllArticlesEvent();
   deleteArticleFromFirebase();
 }
 
