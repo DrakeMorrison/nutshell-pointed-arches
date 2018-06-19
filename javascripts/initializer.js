7f5745bf79873d8154d0862f9bcb8b474c5720a1
Drake Morrison <drakermorrison@gmail.com>
2018-06-19 01:51:32 -0500

    fixed an error ; ;

--- javascripts/initializer.js
+++ javascripts/initializer.js
@@ -1,9 +1,11 @@
 const {exposeTask,} = require('./tasks/taskMain.js');
 const {getAllArticlesEvent,} = require('./articles/article-events.js');
+const {refreshFriends,} = require('./friends/friendEvents.js');
 
 function initializer () {
   exposeTask();
   getAllArticlesEvent();
+  refreshFriends();
 }
 
 module.exports = {
